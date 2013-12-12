b055aff7a6c08ee6aa338d29a7850205826ea2c3
DeProdigy <alex.hint.ah@gmail.com>
2013-12-12 16:48:49 -0500

    created compilations controller, view, show

---
+++ app/controllers/compilations_controller.rb
@@ -0,0 +1,25 @@
+class CompilationsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @compilation = Compilation.create
+
+    redirect_to @compilation
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
