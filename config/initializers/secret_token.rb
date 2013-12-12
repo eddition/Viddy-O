2b47e76be5ee31d3f85936b5b7ea420eb0f63d26
Edward Shin <Eshin0910@gmail.com>
2013-12-11 20:40:09 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ViddyO::Application.config.secret_token = 'd1635db1ebb82efcbbba2fbfafe12d86608a4237b9c7ee96c10592647682d5ecee85c8e0531010e7c7f5ea8b4415e136e44bff62593a4b9e58d7f1826edb44f2'
