300000795656a066662257284f61d1ce4b5ef197
DeProdigy <alex.hint.ah@gmail.com>
2013-12-12 13:11:03 -0500

    added instagram gem

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '3.2.14'
 
 gem 'pg'
 
+gem 'instagram'
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+
+    # ENV['INSTAGRAM_CLIENT_ID']
+    # ENV['INSTAGRAM_CLIENT_SECRET']
+    # raise
   end
 end
