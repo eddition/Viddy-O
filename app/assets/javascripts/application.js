3f531c2a5ffb8346215b37f0c764f2e029964b36
Edward Shin <Eshin0910@gmail.com>
2013-12-12 15:18:16 -0500

    js files added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@
 //
 //= require setup
 //= require routes
-//= require_tree ./templates
+//= require_tree ./collections
 //= require_tree ./models
 //= require_tree ./controllers
 //= require_tree ./views
--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,8 @@ module ViddyO
 
     # Enable the asset pipeline
     config.assets.enabled = true
-# Fix for heroku
+
+    # Fix for heroku
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
