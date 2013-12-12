51efdcfa2f39a76106c3c11429c547351a388fe9
DeProdigy <alex.hint.ah@gmail.com>
2013-12-12 14:00:48 -0500

    pushing to heroku

--- config/application.rb
+++ config/application.rb
@@ -58,12 +58,10 @@ module ViddyO
 
     # Enable the asset pipeline
     config.assets.enabled = true
-# Fix for heroku
+    # Fix for heroku
     config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    #no precompilation of assets
-    config.assets.initialize_on_precompile = false
   end
 end
