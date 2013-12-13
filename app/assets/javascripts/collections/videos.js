c3e4066f033e0cc39a6a6568ff1f64006181df82
DeProdigy <alex.hint.ah@gmail.com>
2013-12-12 21:09:08 -0500

    moved comments

--- app/assets/javascripts/collections/videos.js
+++ app/assets/javascripts/collections/videos.js
@@ -6,6 +6,15 @@ var Videos = Backbone.Collection.extend({
       dataType:   'jsonp'
     });
   },
+  // NEED
+  //  var ajaxFn = $.ajax({
+  //     type: 'get',
+  //     url: "https://api.instagram.com/v1/tags/wdirandom/media/recent?count=1000&client_id=059f1ecfb8df4f0f9e65cbf84e6a1702",
+  //     dataType: "jsonp",
+  //   }).done(function(response){
+  //   this.response = response
+  //   })
+  // ajaxFn gives back an object of videos
 
   model: Video,
 
