22e8adb6b7202c51f6a378191cac2cc673b7da90
Edward Shin <Eshin0910@gmail.com>
2013-12-12 19:41:11 -0500

    edited backbone

---
+++ app/assets/javascripts/collections/videos.js
@@ -0,0 +1,7 @@
+var Videos = Backbone.Collection.extend({
+  initialize: function(hashtag){
+
+    url: "https://api.instagram.com/v1/tags/" + hashtag + "/media/recent?count=1000&client_id=059f1ecfb8df4f0f9e65cbf84e6a1702";
+    this.fetch();
+  }
+});
