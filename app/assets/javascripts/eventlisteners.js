9407fdd47712aa6b6eca840cbe6481e8cdde0dd2
Edward Shin <Eshin0910@gmail.com>
2013-12-16 16:29:25 -0500

    edited divs

--- app/assets/javascripts/eventlisteners.js
+++ app/assets/javascripts/eventlisteners.js
@@ -1,13 +1,20 @@
 function eventListeners() {
-  $('.preview').on('click', function(e){
-    e.preventDefault();
-    $node = $(e.currentTarget.parentNode);
-    var videoLink = $node.data('video_link');
+  $('.preview').on('click', playPreivew);
+  $('#seq-button').on('click', playSeq);
+}
 
-    var currentVideo = $('video:first')[0];
-    currentVideo.src = videoLink;
-    currentVideo.load();
-    currentVideo.play();
+function playPreivew(e){
+  e.preventDefault();
+  $node = $(e.currentTarget.parentNode);
+  var videoLink = $node.data('video_link');
 
-  });
+  var currentVideo = $('video:first')[0];
+  currentVideo.src = videoLink;
+  currentVideo.load();
+  currentVideo.play();
+}
+
+function playSeq(){
+  var videos = $('#sequence-videos')[0].children
+  debugger;
 }
