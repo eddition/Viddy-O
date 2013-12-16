02467ef6a9cb5f84914637ec41f704795be1a05f
DeProdigy <alex.hint.ah@gmail.com>
2013-12-16 14:20:13 -0500

    working preview

--- app/assets/javascripts/eventlisteners.js
+++ app/assets/javascripts/eventlisteners.js
@@ -2,6 +2,12 @@ function eventListeners() {
   $('.preview').on('click', function(e){
     e.preventDefault();
     $node = $(e.currentTarget.parentNode);
-    console.log($node.data('video_link'));
+    var videoLink = $node.data('video_link');
+
+    var currentVideo = $('video:first')[0];
+    currentVideo.src = videoLink;
+    currentVideo.load();
+    currentVideo.play();
+
   });
 }
--- todo.md
+++ todo.md
@@ -2,7 +2,7 @@
 
 ~~Show video should pass in the name of the hashtag~~
 
-main video player should be loaded and on click of preview, it auto plays the video
+~~main video player should be loaded and on click of preview, it auto plays the video~~
 
 ~~clear input form after submission~~
 
