e6cd4e66c2d451e15402609cb8ea06c7d5c7af1b
DeProdigy <alex.hint.ah@gmail.com>
2013-12-16 17:29:28 -0500

    paySeq working

--- app/assets/javascripts/eventlisteners.js
+++ app/assets/javascripts/eventlisteners.js
@@ -8,13 +8,35 @@ function playPreivew(e){
   $node = $(e.currentTarget.parentNode);
   var videoLink = $node.data('video_link');
 
-  var currentVideo = $('video:first')[0];
-  currentVideo.src = videoLink;
-  currentVideo.load();
-  currentVideo.play();
+  var videoPlayer = $('video:first')[0];
+  videoPlayer.src = videoLink;
+  videoPlayer.load();
+  videoPlayer.play();
 }
 
 function playSeq(){
-  var videos = $('#sequence-videos')[0].children;
-  debugger;
+  var $videos = $('#sequence-videos').children('li');
+  //put all of the sequence links into an array
+  var videoLinks = _.map($videos, function(video){
+    return $(video).data('video_link');
+  });
+
+  var videoPlayer = $('video:first')[0];
+  var i = 0;
+  videoPlayer.src = videoLinks[i];
+  videoPlayer.load();
+  // debugger
+  videoPlayer.play();
+
+  $(videoPlayer).on('ended', function(){
+    ++i;
+    if (i < videoLinks.length){
+      videoPlayer.src = videoLinks[i];
+      videoPlayer.load();
+      videoPlayer.play();
+    } else {
+      console.log('ended preview');
+    }
+  });
+
 }
